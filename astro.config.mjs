d5b505de46ed36df8bc0e7f2c969a63fcbe5727c
Rohid <rohidul209@gmail.com>
2022-11-13 12:57:43 +0600

    base layouts and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+import readingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://rohid.dev",
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkReadingTime],
+  },
+  integrations: [tailwind(), sitemap(), mdx()],
+});
+
+function remarkReadingTime() {
+  return function (tree, file) {
+    const textOnPage = toString(tree);
+    file.data.astro.frontmatter.readingTime = readingTime(textOnPage).text;
+  };
+}
